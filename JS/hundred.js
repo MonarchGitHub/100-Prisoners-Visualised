99a7f72846617a6efa36819cfcd326d7d4167dea
Monarch <monarchk7@gmail.com>
2022-08-07 00:47:26 +0530

    updated js

--- JS/hundred.js
+++ JS/hundred.js
@@ -1,1 +1,9 @@
 console.log("Working");
+// let prisoner_number=0;
+// const number = [];
+// let counter=100;
+// let chosen=0;
+// while(counter){
+//     chosen=Math.floor(Math.random() * 101 - chosen);
+//     console.log(chosen);
+// }
